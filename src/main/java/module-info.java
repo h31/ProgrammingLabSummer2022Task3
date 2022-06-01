9cb00f960b76d0a4e99f821afe706f5cdd388456
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2022-06-02 01:12:45 +0300

    Added .jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,6 @@ module com.project.seabattle {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-
     opens com.project.seabattle to javafx.fxml;
     exports com.project.seabattle;
 }
